099a52fff9844ca878f00455d5afd7ec4f95132a
Christina Lee <christina.lwt@gmail.com>
2015-02-27 17:05:52 +0800

    Did more test to check search function is working.

--- TextBuddy/src/TextBuddyTest.java
+++ TextBuddy/src/TextBuddyTest.java
@@ -12,15 +12,16 @@ public class TextBuddyTest {
 		//to create a text file for testing
 		TextBuddy.checkFileExist();
 
-	//	assertEquals("\nadded to mytextfile.txt : \"software eng\"\n", TextBuddy.executeCommand("add software eng"));
-	//	assertEquals("\nadded to mytextfile.txt : \"christina lee\"\n", TextBuddy.executeCommand("add christina lee"));
+		//test sort
+		assertEquals("\nadded to mytextfile.txt : \"software eng\"\n", TextBuddy.executeCommand("add software eng"));
+		assertEquals("\nadded to mytextfile.txt : \"christina lee\"\n", TextBuddy.executeCommand("add christina lee"));
 
-	//	TextBuddy.executeCommand("display");
-	//	TextBuddy.executeCommand("sort");
-	//	TextBuddy.executeCommand("display");
+		TextBuddy.executeCommand("display");
+		TextBuddy.executeCommand("sort");
+		TextBuddy.executeCommand("display");
 		
-	
-		TextBuddy.executeCommand("search chr");
+		//test search
+		TextBuddy.executeCommand("search abc");
 
 	}
 	
