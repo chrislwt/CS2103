7e0c84b712cc08c9fce38677e950a1b6bfd1b4e9
Christina Lee <christina.lwt@gmail.com>
2015-02-27 16:55:29 +0800

    Added search function and TDD, not tested yet.

--- TextBuddy/src/TextBuddyTest.java
+++ TextBuddy/src/TextBuddyTest.java
@@ -6,17 +6,24 @@ import org.junit.Test;
 
 public class TextBuddyTest {
 	
+	
 	@Test
-	public void testSort() {
+	public void testExcecuteCommand() {
 		//to create a text file for testing
 		TextBuddy.checkFileExist();
 
-		assertEquals("\nadded to mytextfile.txt : \"software eng\"\n", TextBuddy.executeCommand("add software eng"));
-		assertEquals("\nadded to mytextfile.txt : \"christina lee\"\n", TextBuddy.executeCommand("add christina lee"));
+	//	assertEquals("\nadded to mytextfile.txt : \"software eng\"\n", TextBuddy.executeCommand("add software eng"));
+	//	assertEquals("\nadded to mytextfile.txt : \"christina lee\"\n", TextBuddy.executeCommand("add christina lee"));
 
-		TextBuddy.executeCommand("display");
-		TextBuddy.executeCommand("sort");
-		TextBuddy.executeCommand("display");
+	//	TextBuddy.executeCommand("display");
+	//	TextBuddy.executeCommand("sort");
+	//	TextBuddy.executeCommand("display");
+		
+	
+		TextBuddy.executeCommand("search soft");
+
 	}
+	
+
 
 }
